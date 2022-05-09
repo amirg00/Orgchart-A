4a66855e4133b52933786e00c1964abb19521319
AmirGill <amirg00@walla.com>
2022-05-10 00:53:08 +0300

    Upload almost full implementation for OrgChart.cpp

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -1,38 +1,41 @@
 #include "OrgChart.hpp"
 
-ostream& ariel::operator<<(ostream &os, const ariel::OrgChart &chart) {
 
-}
+//ostream& ariel::operator<<(ostream &os, const ariel::OrgChart &chart) {
+//
+//}
 
-void ariel::OrgChart::add_root(const string &root_val) {
+ariel::OrgChart& ariel::OrgChart::add_root(const string &root_val) {
     _chart.add_root(root_val);
+    return *this;
 }
 
-void ariel::OrgChart::add_sub(const string &super, const string &sub) {
+ariel::OrgChart& ariel::OrgChart::add_sub(const string &super, const string &sub) {
     _chart.add_sub(super, sub);
+    return *this;
 }
 
-Tree<string>::level_order_iterator ariel::OrgChart::begin_level_order() {
-    return Tree<string>::begin_level_order();
+Tree<string>::level_order_iterator ariel::OrgChart::begin_level_order(){
+    return _chart.begin_level_order();
 }
 
 Tree<string>::level_order_iterator ariel::OrgChart::end_level_order() {
-    return Tree<string>::level_order_iterator();
+    return _chart.end_level_order();
 }
 
 Tree<string>::reverse_level_order_iterator ariel::OrgChart::begin_reverse_order() {
-    return Tree<string>::reverse_level_order_iterator();
+    return _chart.begin_reverse_order();
 }
 
 Tree<string>::reverse_level_order_iterator ariel::OrgChart::end_reverse_order() {
-    return Tree<string>::reverse_level_order_iterator();
+    return  _chart.end_reverse_order();
 }
 
 Tree<string>::preorder_iterator ariel::OrgChart::begin_preorder() {
-    return Tree<string>::preorder_iterator();
+    return  _chart.begin_preorder();
 }
 
 Tree<string>::preorder_iterator ariel::OrgChart::end_preorder() {
-    return Tree<string>::preorder_iterator();
+    return  _chart.end_preorder();
 }
 
